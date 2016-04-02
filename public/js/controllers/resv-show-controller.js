894e4d041dda22cd714d4b5edf0a2cbe8230ac93
hagary <hagar.y.omar@gmail.com>
2016-04-02 22:59:03 +0200

    controller&service initial trial

--- public/js/controllers/resv-show-controller.js
+++ public/js/controllers/resv-show-controller.js
@@ -1,5 +1,31 @@
 angular.module('alaska')
-.controller("ResvShowController", function(resvSrvc){
-	var controller = this;
-	
-})
+.controller("ResvShowController", function($scope,$routeParams,resvSrvc){
+
+	resvSrvc.getResv($routeParams.id, function(reservation){
+		$scope.resv = reservation;
+		resvSrvc.getoutgoingFlight(function(outgoingFlight){
+			$scope.outFlight = outgoingFlight;
+			resvSrvc.getreturnFlight(function(returnFlight){
+				$scope.retFlight = returnFlight;
+					resvSrvc.getoriginAirpot(outgoingFlight, function(originOut){
+						$scope.outOrigin = originOut;
+						resvSrvc.getdestAirpot(outgoingFlight, function(destOut){
+							$scope.outDest = destOut;
+							resvSrvc.getoriginAirpot(returnFlight, function(originRet){
+								$scope.retOrigin = originRet;
+								resvSrvc.getdestAirpot(returnFlight, function(destRet){
+									$scope.retDest = destRet;
+									resvSrv.getseat(outgoingFlight , function (outSeat) {
+										$scope.outSeat  = outSeat;
+										resvSrv.getseat(returnFlight, function(seatRet){
+											$scope.retSeat = seatRet;
+										})
+									})
+								});
+							});
+						});
+				});
+			});
+		});
+	});
+});
--- public/js/services/resv-info-service.js
+++ public/js/services/resv-info-service.js
@@ -1,15 +1,84 @@
 angular.module('alaska')
 .factory("resvSrvc", function ($http){
 	return {
-		getResv : function(resvNo){
+		getResv : function(resvNo,cb){
+			var resv;
+			$http.get('dummyData/bookings.json').success(function(bookings){
+					for(var i=0 ; i<bookings.length ; i++){
+						if(bookings[i].reservationID == resvNo){
+							resv = bookings[i];
+							this.booking=resv;
+							break;
+						}
+					}
+					cb(resv);
+			}
+			);
+		},
+		getoutgoingFlight : function(cb){ //get outgoing flight of current booking
+			$http.get('dummyData/flights.json').success(function(flights){
+				getResv
+				var flight;
+				for(var i=0 ; i<flights.length ; i++){
+					if(flights[i].flightNumber == this.booking.outgoingFlight){
+						flight = flights[i];
+						break;
+					}
+				}
+				cb(flight);
+			})
+		},
+		getreturnFlight : function(cb){ //get return flight of current booking
+			$http.get('dummyData/flights.json').success(function(flights){
+				var flight;
+				for(var i=0 ; i<flights.length ; i++){
+					if(flights[i].flightNumber == this.booking.returnFlight){
+						flight = flights[i];
+						break;
+					}
+				}
+				cb(flight);
+			});
+		},
 
+		getoriginAirpot : function(flight,cb){
+			$http.get('dummyData/airports.json').success(function(airports){
+				var airport;
+				for (var i = 0; i < airports.length; i++) {
+					this.get
+					if(airports[i].iata == flight.origin){
+						airport = airports[i];
+						break;
+					}
+				}
+				cb(airport);
+			});
 
 		},
-		getFlight : function(flightNo){
-		},
-		getAirpot : function(iata){
+		getdestAirpot : function(flight,cb){
+			$http.get('dummyData/airports.json').success(function(airports){
+				var airport;
+				for (var i = 0; i < airports.length; i++) {
+					this.get
+					if(airports[i].iata == flight.destination){
+						airport = airports[i];
+						break;
+					}
+				}
+				cb(airport);
+			});
 
 		}
-
-	}
+		,
+		getseatNumber : function(flight, cb){
+			var seat;
+			var cabin;
+			for (var i = 0; i < flight.seatmap.length; i++) {
+				if(flight.seatmap[i].reservationID == this.booking.reservationID){
+					seat = flight.seatmap[i];
+					break;
+				}
+			}
+			cb(seat);
+		}
 });
