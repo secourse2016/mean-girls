3645df6e221a5faba006b5a784224cc8d00c24bf
Hadeel <hadeelahmed17795@gmail.com>
2016-04-03 16:48:40 +0200

    Index,Core & passengerInfo

---
+++ public/js/core.js
@@ -0,0 +1,10 @@
+var app = angular.module('alaska',['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider.when('/passengerInfo',{
+
+		templateUrl: '/views/passengerInfo.html',
+		//controller: ''
+	}
+)});
+
