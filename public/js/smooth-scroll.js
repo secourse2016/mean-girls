eca5518172f892510ae98c2dcec8321fec4c3731
hagary <hagar.y.omar@gmail.com>
2016-04-10 03:06:33 +0200

    scroll navbar added

---
+++ public/js/smooth-scroll.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function(){
+
+    });
+  });
+});
