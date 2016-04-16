a33179139dd9ce41979fe91bd6241cd807037c1a
Rana Tarek Hassan <roona955@gmail.com>
2016-04-16 20:44:09 +0200

    removed temp seed method to be replaced later

--- db.js
+++ db.js
@@ -3,9 +3,6 @@ var dbURL = 'mongodb://localhost:27017/alaska';
 var DB=null;
 
 
-var flights=require('./dummyData/flights.json');
-var bookings=require('./dummyData/bookings.json');
-
 exports.connect = function (cb){
 	mongo.connect(dbURL, function (err,db){
 			if(err)
@@ -17,56 +14,13 @@ exports.connect = function (cb){
 			cb(err,db);
 		});
 }
-seedFlights = function(cb){
-	DB.collection("flights").count(function (err,i){
-		if(i>0){
-			console.log("error");
-			cb(err,false);
-		}
-		else{
-			DB.collection("flights").insert(flights,function(err,seeded){
-				cb(err,seeded);
-			});
-			
-		}
-		
-	});
-}
-
-seedBooking = function(cb){
-	DB.collection("bookings").count(function (err,i){
-		if(i>0){
-			console.log("error");
-			cb(err,false);
-		}
-		else{
-			DB.collection("bookings").insert(bookings,function(err,seeded){
-				cb(err,seeded);
-			});
-			
-		}
-		
-	});
-}
 
 
 // connect(function (cb){
-// 	seedFlights(function(cb){
-// 		seedBooking(function(cb){
 
-// 			searchFlight("AB123",function(err,flight){
-// 				 console.log(flight);
-// 			});
-// 			searchBooking("hy123",function(err,booking){
-// 				 console.log(booking);
-// 			});
-
-// 		});
-// 	});
 // });
 
-
-
+//Find flight from DB when given flight number
 exports.searchFlight = function(flightNo,cb){
 	DB.collection('flights').find({"flightNumber":flightNo},function(err,cursor){
 		cursor.toArray(cb);
@@ -75,7 +29,7 @@ exports.searchFlight = function(flightNo,cb){
 }
 
 
-
+//Find booking from DB when given booking reference number
 exports.searchBooking = function(bookingRef,cb){
 	DB.collection('bookings').find({"bookingRefNo":bookingRef},function(err,cursor){
 		cursor.toArray(cb);
