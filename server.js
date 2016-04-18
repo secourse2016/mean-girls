75ab9d6329e0b1e57e143eb7b9c8851f38201955
Rana Tarek Hassan <roona955@gmail.com>
2016-04-18 20:14:22 +0200

    returned port to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var db=require("./db.js");
 
 db.connect(function(){
  	var app=require("./app/app");
- 	app.listen(3000, function(){
+ 	app.listen(80, function(){
 		console.log("Server up & listening on 80");
 	});
 
