46b1381d29940b4ccd1d182b80736ce4d5ef868d
Hagar Yasser <hagar.y.omar@gmail.com>
2016-04-19 03:16:09 +0200

    resolved conflicts with dev

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-var app=require("./app");
-app.listen(3000, function(){
-=======
 var express = require('express');
 var db = require('./db.js');
->>>>>>> 6e813c0f92b36b094b469946119e70510b156c1a
 
 db.connect(function(){
 	var app=require("./app/app.js");
@@ -13,4 +8,3 @@ db.connect(function(){
 		console.log("Server up & listening on 3000");
 	});
 });
-
