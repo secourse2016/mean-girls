a182a9a239e2e7f8731f3cc23a72eac6014980c2
root <root@ip-172-31-51-242.ec2.internal>
2016-04-19 22:33:23 +0000

    hard coding port in server

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var express = require('express');
 db.connect(function(){
 	var app=require("./app/app.js");
 	db.seed(function(){
-		app.listen(process.env.PORT, function(){
-			console.log("Server up & listening on "+process.env.PORT);
+		app.listen(80, function(){
+			console.log("Server up & listening on "+80);
 		});
 	});
 
