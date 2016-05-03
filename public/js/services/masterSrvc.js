62b585c3c816d5fa573764a2fa20c532e7c5f4c5
Hagar Yasser <hagar.y.omar@gmail.com>
2016-05-03 21:31:29 +0200

    stripe & booking for any round tripe DONE

--- public/js/services/masterSrvc.js
+++ public/js/services/masterSrvc.js
@@ -1,4 +1,4 @@
-angular.module('alaska').factory('masterSrvc', function ($http,$location,bookingSrvc,modalSrvc,$uibModal) {
+angular.module('alaska').factory('masterSrvc', function ($http,$location,bookingSrvc,modalSrvc,$uibModal,confirmSrvc) {
   return {
     Confirm : function(){
 
@@ -9,25 +9,21 @@ angular.module('alaska').factory('masterSrvc', function ($http,$location,booking
       var expiryYear    = this.payment.expiryYear;
       var returnFlight  = this.returnFlight;
       var outgoingFlight= this.outgoingFlight;
+      var emptyArr  = [];
+      var data={};
+
+      data.passengerDetails = emptyArr.push(angular.copy(this.passenger));
+      data.class            = this.seatClass;
+      data.outgoingFlightId = angular.copy(this.outgoingFlight.flightId);
+      data.returnFlightId   = angular.copy(this.returnFlight.flightId);
+      data.cost             = this.outgoingFlight.cost + this.returnFlight.cost;
+      this.data=data;
 
       if(returnFlight.Airline === outgoingFlight.Airline){
 
-        if(returnFlight.Airline === "Alaska"){
-
-          Stripe.card.createToken({
-            number: cardNo,
-            cvc: cvc,
-            exp_month: expiryMonth,
-            exp_year: expiryYear
-          },
-          $scope.roundAlaska
-        );
-      }
-      else
-      {
-        //round trip by another Airline
         var airlineIP = returnFlight.airlineIP;
-        $http.get(airlineIP + '/stripe/pubkey' , function(pubkey){
+
+        $http.get(airlineIP + '/stripe/pubkey').success(function(pubkey){
           Stripe.setPublishableKey(pubkey);
           Stripe.card.createToken({
             number: cardNo,
@@ -35,52 +31,123 @@ angular.module('alaska').factory('masterSrvc', function ($http,$location,booking
             exp_month: expiryMonth,
             exp_year: expiryYear
           },
-          $scope.roundOther
+          this.roundSameAirline
         );
-      }
+      });
+
     }
+    else {     //flights by diff. airlines
+      //create 2 tokens, one for each airline
+      var outAirlineIP = outgoingFlight.airlineIP;
+      var retAirlineIP = returnFlight.airlineIP;
+
+      $http.get(outAirlineIP + '/stripe/pubkey').success(function(pubkey){
+        Stripe.setPublishableKey(pubkey);
+        Stripe.card.createToken({
+          number: cardNo,
+          cvc: cvc,
+          exp_month: expiryMonth,
+          exp_year: expiryYear
+        },
+        function(status,response){
+          if(response.error){
+            // Problem!
+            this.openModal("Error occured during card verification: "+ response.error);
+            return;
+          }
+          var outToken = response.id;
+          $http.get(retAirlineIP + '/stripe/pubkey').success(function(pubkey){
+            Stripe.setPublishableKey(pubkey);
+            Stripe.card.createToken(
+              {
+                number: cardNo,
+                cvc: cvc,
+                exp_month: expiryMonth,
+                exp_year: expiryYear
+              },function (status1, response1) {
+                Stripe.setPublishableKey('pk_test_I5BoepTFhbNEZbcMq5eUeSRg'); //return to MY pubkey
+                if(response1.error){
+                  // Problem!
+                  this.openModal("Error occured during card verification: "+ response1.error);
+                  return;
+                }
+
+                var retToken = response1.id;
+
+                //now ready to issue 2 post requests
+                //modify post request body
+                //for outgoing
+                var outReq = angular.copy(data);
+                outReq.paymentToken = outToken;
+                outReq.returnFlightId = null;
+                $http.post(outAirlineIP + '/booking', outReq).success(function (resOut) {
+                  if(resOut.errorMessage){
+                    //couldn't charge the card
+                    this.openModal(resOut.errorMessage);
+                    return;
+                  }
+                  confirmSrvc.bookingRefOut = resOut.refNum;
+
+                  //modify for return
+                  var retReq = angular.copy(data);
+                  retReq.paymentToken     = retToken;
+                  retReq.outgoingFlightId = data.returnFlightId;
+                  retReq.returnFlightId   = null;
+                  $http.post(retAirlineIP + '/booking',retReq).success(function (resRet){
+                    if(resRet.errorMessage){
+                      //couldn't charge the card
+                      this.openModal(resRet.errorMessage);
+                      return;
+                    }
+                    confirmSrvc.bookingRefRet = resRet.refNum;
+                    //done
+                    location.url('/confirm');
+                  })
+                }
+
+              }
+
+            })
+          });
+        }
+      );
+    });
   }
 }
-roundOther : function (status, response){
-  
+roundDiffAirline : function (){
+
+
 
 }
 
-roundAlaska: function(status,response){
+roundSameAirline : function (status, response){
+  Stripe.setPublishableKey('pk_test_I5BoepTFhbNEZbcMq5eUeSRg');
   if (response.error) {
     // Problem!
-    $scope.openModal("Error occured during card verification: "+ response.error);
+    this.openModal("Error occured during card verification: "+ response.error);
     return;
   }
   // Token was created!
   // Get the token ID:
   console.log("hallelujah");
   var token = response.id;
-  var emptyArr  = [];
-  var data={};
+  this.data.paymentToken     = token;
 
-  data.paymentToken     = token;
-  data.passengerDetails = emptyArr.push(angular.copy(this.passenger));
-  data.class            = this.seatClass;
-  data.outgoingFlightId = angular.copy(this.outgoingFlight.flightId);
-  data.returnFlightId   = angular.copy(this.returnFlight.flightId);
-  data.cost             = this.outgoingFlight.cost + this.returnFlight.cost;
-
-  $http.post('/api/booking',data).success(function(res){
-    //couldn't charge the card
+  $http.post(airlineIP + '/booking',data).success(function(res){
     if(res.errorMessage){
+      //couldn't charge the card
       this.openModal(res.errorMessage);
       return;
     }
-
     var bookingRef = res.refNum;
-    $http.get('/api/booking/'+bookingRef).success(function(booking){
-      bookingSrvc.booking=booking;
-      $location.url('/booking');
-    });
-
+    confirmSrvc.bookingRefOut = res.refNum;
+    confirmSrvc.bookingRefRet = null;
+    location.url('/confirm');
   });
+
 }
+
+
 
 openModal:function(message){
   modalSrvc.modalMessage = message;
