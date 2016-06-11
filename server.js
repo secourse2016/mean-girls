43892dc748509c131ffa7f7d1440a83336f82cd1
Hagar Yasser <hagar.y.omar@gmail.com>
2016-06-11 23:40:59 +0200

    deployment port modified

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 
 var db=require("./db.js");
 var express = require('express');
-
+var port = process.env.PORT || 8080;
 db.connect(function(){
 	var app=require("./app/app.js");
 	db.seed(function(){
-		app.listen(80, function(){
+		app.listen(port, function(){
 			console.log("Server up & listening on "+80);
 		});
 	});
